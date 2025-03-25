94844c17b9329555ef7b5aaecafd0acba9608b7e
Imtiaz <mdimtiazzihad@gmail.com>
2025-03-25 18:57:22 +0600

    Landing page Design done

--- app/dashboard/_components/AddCourse.jsx
+++ app/dashboard/_components/AddCourse.jsx
@@ -1,11 +1,19 @@
+"use client"
+import { useUser } from '@clerk/nextjs'
 import React from 'react'
+import { Button } from '../../../components/ui/button';
 
 function AddCourse() {
+  const {user} =useUser();
   return (
-    <div>
+    <div className='flex justify-between items-center '>
       <div>
-        <h2>Hello, {}</h2>
+        <h2 className='text-2xl'>Hello,
+          <span className='font-bold'>{user?.fullName}</span> </h2>
+
+          <p className='text-sm text-gray-500'>Create new Course With AI, Expend Your knowledge </p>
       </div>
+      <Button>+ Create AI Course</Button>
     </div>
   )
 }
